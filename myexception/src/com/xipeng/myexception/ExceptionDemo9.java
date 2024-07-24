0748340451cabd59a425dbd4c46dc959ea0ffca4
xipeng <sxp0728@126.com>
2024-07-23 22:06:01 -0400

    p59

--- myexception/src/com/xipeng/myexception/ExceptionDemo9.java
+++ myexception/src/com/xipeng/myexception/ExceptionDemo9.java
@@ -4,6 +4,7 @@ public class ExceptionDemo9 {
     public static void main(String[] args) {
         // 3. 如果try中遇到的问题没有被捕获，怎么执行？
         //    相当于try catch代码白写了，交给虚拟机进行处理
+
         int[] arr = {1, 2, 3, 4, 5};
 
         try {
