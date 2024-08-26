4807031b0d691fc1ecde3354727e43ab9dbe3888
xipeng <sxp0728@126.com>
2024-08-26 19:31:16 -0400

    p149

--- mythread/src/com/xipeng/a09threadsafe1/MyThread.java
+++ mythread/src/com/xipeng/a09threadsafe1/MyThread.java
@@ -4,19 +4,25 @@ public class MyThread extends Thread {
     // 这个类所有对象共享ticket数据
     static int ticket = 0;
 
+    // 锁对象，一定要是唯一的
+
     @Override
     public void run() {
         while (true) {
-            if (ticket < 100) {
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
+            // 同步代码块
+            // 当一个线程执行次代码时，其他线程不能执行此代码
+            synchronized (MyThread.class) {
+                if (ticket < 100) {
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    ticket++;
+                    System.out.println(getName() + "正在卖第" + ticket + "张票");
+                } else {
+                    break;
                 }
-                ticket++;
-                System.out.println(getName() + "正在卖第" + ticket + "张票");
-            } else {
-                break;
             }
         }
     }
