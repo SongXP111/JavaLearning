77566677ab46f1a28db38c040dc43f6540663efe
xipeng <sxp0728@126.com>
2024-09-02 19:50:17 -0400

    p196

--- myreflect/src/com/xipeng/myreflect4/MyReflectDemo.java
+++ myreflect/src/com/xipeng/myreflect4/MyReflectDemo.java
@@ -1,7 +1,65 @@
 package com.xipeng.myreflect4;
 
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+
 public class MyReflectDemo {
     public static void main(String[] args) throws Exception {
         // 反射获取成员方法
+
+        // 1. 获取class对象
+        Class clazz = Class.forName("com.xipeng.myreflect4.Student");
+
+        // 2. 获取成员方法
+
+        // 获取公共方法（也会获取到父类的方法）
+//        Method[] methods = clazz.getMethods();
+//        for (Method method : methods) {
+//            System.out.println(method);
+//        }
+
+        // 获取所有的方法对象（不能获取父类的，但可以获取本类私有的）
+//        Method[] methods = clazz.getDeclaredMethods();
+//        for (Method method : methods) {
+//            System.out.println(method);
+//        }
+
+        // 因为方法重载，需要明确方法参数的类
+//        Method m = clazz.getMethod("eat", String.class);
+//        System.out.println(m);
+
+        Method m = clazz.getDeclaredMethod("eat", String.class);
+        System.out.println(m);
+
+        // 获取方法的修饰符
+        int mod = m.getModifiers();
+        System.out.println(mod);
+
+        // 获取方法的名字
+        String name = m.getName();
+        System.out.println(name);
+
+        // 获取方法的形参
+        Parameter[] parameters = m.getParameters();
+        for (Parameter parameter : parameters) {
+            System.out.println(parameter);
+        }
+
+        // 方法抛出的异常
+        Class[] exceptions = m.getExceptionTypes();
+        for (Class exception : exceptions) {
+            System.out.println(exception);
+        }
+
+        // 运行获取出来的方法
+        Student s = new Student();
+        /*
+        * 参数：方法的调用者，和调用方法的实际参数
+        * */
+        m.setAccessible(true);
+        Object result = m.invoke(s, "汉堡包");
+        System.out.println(result);
+
+        // 获取方法的返回值
     }
 }
--- myreflect/src/com/xipeng/myreflect4/Student.java
+++ myreflect/src/com/xipeng/myreflect4/Student.java
@@ -1,18 +1,17 @@
 package com.xipeng.myreflect4;
 
+import java.io.IOException;
+
 public class Student {
     private String name;
     private int age;
-    public String gender;
-
 
     public Student() {
     }
 
-    public Student(String name, int age, String gender) {
+    public Student(String name, int age) {
         this.name = name;
         this.age = age;
-        this.gender = gender;
     }
 
     /**
@@ -47,23 +46,16 @@ public class Student {
         this.age = age;
     }
 
-    /**
-     * 获取
-     * @return gender
-     */
-    public String getGender() {
-        return gender;
-    }
-
-    /**
-     * 设置
-     * @param gender
-     */
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
     public String toString() {
-        return "Student{name = " + name + ", age = " + age + ", gender = " + gender + "}";
+        return "Student{name = " + name + ", age = " + age + "}";
+    }
+
+    public void sleep() {
+        System.out.println("睡觉");
+    }
+
+    private String eat(String something) throws IOException, ClassNotFoundException {
+        System.out.println("在吃" + something);
+        return "好吃";
     }
 }
