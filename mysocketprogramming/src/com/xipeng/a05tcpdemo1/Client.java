6af4bfd922cf48842b58a0d574c193404b09c263
xipeng <sxp0728@126.com>
2024-09-02 14:18:30 -0400

    p181

---
+++ mysocketprogramming/src/com/xipeng/a05tcpdemo1/Client.java
@@ -0,0 +1,25 @@
+package com.xipeng.a05tcpdemo1;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class Client {
+    public static void main(String[] args) throws IOException {
+        // TCP协议，发送数据
+
+        // 1. 创建Socket对象
+        // 细节：在创建对象的时候会链接服务端
+        // 如果链接不上，代码会报错
+        Socket socket = new Socket("127.0.0.1", 10000);
+
+        // 2. 可以从链接通道中获取输出流
+        OutputStream outputStream = socket.getOutputStream();
+        // 写出数据
+        outputStream.write("aaa".getBytes());
+
+        // 3. 释放资源
+        outputStream.close();
+        socket.close();
+    }
+}
