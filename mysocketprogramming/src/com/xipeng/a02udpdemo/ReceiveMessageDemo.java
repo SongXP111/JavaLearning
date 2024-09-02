977464c9532a6b457c00244f8a569182a2fde7e2
xipeng <sxp0728@126.com>
2024-09-01 22:55:53 -0400

    p177

---
+++ mysocketprogramming/src/com/xipeng/a02udpdemo/ReceiveMessageDemo.java
@@ -0,0 +1,35 @@
+package com.xipeng.a02udpdemo;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
+public class ReceiveMessageDemo {
+    public static void main(String[] args) throws IOException {
+        // 接收数据
+
+        // 1. 创建DatagramSocket对象（快递公司）
+        // 细节：
+        // 接收的时候一定要绑定端口，且必须和发送的端口一致
+        DatagramSocket ds = new DatagramSocket(10086);
+
+        // 2. 接收数据包
+        byte[] bytes = new byte[1024];
+        DatagramPacket dp = new DatagramPacket(bytes, bytes.length);
+        ds.receive(dp);
+
+        // 3. 解析数据包
+        byte[] data = dp.getData();
+        int len = dp.getLength();
+        InetAddress address = dp.getAddress();
+        int port = dp.getPort();
+
+        System.out.println("接收到数据:" + new String(data, 0, len));
+        System.out.println("该数据是从" + address + ":" + port + "发出的");
+
+        // 4. 释放资源
+        ds.close();
+    }
+}
---
+++ mysocketprogramming/src/com/xipeng/a02udpdemo/SendMessageDemo.java
@@ -0,0 +1,31 @@
+package com.xipeng.a02udpdemo;
+
+import java.io.IOException;
+import java.net.*;
+
+public class SendMessageDemo {
+    public static void main(String[] args) throws IOException {
+        // 发送数据
+
+        // 1. 创建DatagramSocket对象（快递公司）
+        // 细节：
+        // 绑定端口：通过这个端口往外发送
+        // 空参：随机端口
+        // 有参：绑定端口
+        DatagramSocket ds = new DatagramSocket();
+
+        // 2. 打包数据
+        String str = "你好呀！！！";
+        byte[] bytes = str.getBytes();
+        InetAddress address = InetAddress.getByName("127.0.0.1");
+        int port = 10086;
+
+        DatagramPacket dp = new DatagramPacket(bytes, bytes.length, address, port);
+
+        // 3. 发送数据
+        ds.send(dp);
+
+        // 释放资源
+        ds.close();
+    }
+}
